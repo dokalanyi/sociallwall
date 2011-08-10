38f7c72df40c9c414a420517b0b1fbfcf18caa35
Okalany Daniel <dokalanyi@gmail.com>
2011-08-10 09:28:52 +0300

    Fixed sql file

--- socialwall.sql
+++ socialwall.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 14, 2011 at 03:21 PM
+-- Generation Time: Aug 10, 2011 at 10:27 AM
 -- Server version: 5.0.67
 -- PHP Version: 5.2.6
 
@@ -31,14 +31,6 @@ CREATE TABLE IF NOT EXISTS `groups` (
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
---
--- Dumping data for table `groups`
---
-
-INSERT INTO `groups` (`id`, `name`) VALUES
-(1, 'ROOT'),
-(2, 'users');
-
 -- --------------------------------------------------------
 
 --
@@ -60,13 +52,6 @@ CREATE TABLE IF NOT EXISTS `tokens` (
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
---
--- Dumping data for table `tokens`
---
-
-INSERT INTO `tokens` (`id`, `fb_id`, `fb_at`, `fb_ts`, `fb_sk`, `tw_id`, `tw_at`, `tw_ts`, `gb_at`, `gb_ts`, `user_id`) VALUES
-(2, 1273114438, '126199764091804|3b6ecc7d49706c1fde1c3647-1273114438|lek-PF06AabHbWrVUagkmEsYywo', '9b93730d0d837df70878d9279cb81217', '3b6ecc7d49706c1fde1c3647-1273114438', '', '', '', '', '', '115');
-
 -- --------------------------------------------------------
 
 --
@@ -78,12 +63,7 @@ CREATE TABLE IF NOT EXISTS `used` (
   `user_id` varchar(30) NOT NULL,
   `photo_id` varchar(30) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `used`
---
-
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -106,10 +86,3 @@ CREATE TABLE IF NOT EXISTS `users` (
   `group_id` int(1) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=119 ;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `firstName`, `lastName`, `userName`, `password`, `phoneNumber`, `country`, `sex`, `live`, `activated`, `regtime`, `group_id`) VALUES
-(115, 'Okalany', 'Daniel', 'dokalanyi', '34b57b639d614a6c627ee3c5bea50c52', '0773023366', 'Uganda', 'male', 1, 1, '2010-06-30 12:13:06', 1);
